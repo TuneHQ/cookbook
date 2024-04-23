1eca4bc87d31ee941b71d8badc2e0d81c2eb2d6c
Payal sahu <115584065+Payalsahuj@users.noreply.github.com>
2024-04-23 15:49:19 +0530

    chore(ChatExample): Added messagecard section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { useEffect, useRef, useState } from "react";
 import { Pause, Send, TextArea } from "chainfury";
-import { useState } from "react";
-
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehype from "rehype-raw";
 export interface ChatInterface {
   msg: string;
   attachment_name?: string;
@@ -10,14 +12,18 @@ export interface ChatInterface {
   message_id?: string;
 }
 
-const faqs = [
-  "Question 1?",
-  "Question 2?",
-  "Question 3?",
-  "Question 4?",
-  "Question 5?",
-  "Question 6?",
-];
+export interface ModelInterface {
+  id: number;
+  name: string;
+  can_modify: boolean;
+  is_running: boolean;
+  config?: any;
+  nbx_key?: string;
+  deployment_id?: string;
+  model_id?: string;
+  is_shared?: boolean;
+  provider?: string;
+}
 
 const scrollToBottom = (force = false) => {
   const chatsHolder = document.querySelector(".chatsHolder");
@@ -34,16 +40,134 @@ const scrollToBottom = (force = false) => {
   }
 };
 
+const faqs = [
+  "Question 1?",
+  "Question 2?",
+  "Question 3?",
+  "Question 4?",
+  "Question 5?",
+  "Question 6?",
+];
+
 export default function Home() {
   const [chats, setChats] = useState<ChatInterface[]>([]);
-  const [search, setSearch] = useState("");
+  const [answer, setAnswer] = useState("");
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  let abortController = useRef(new AbortController());
 
+  const stopGenerating = () => {
+    if (abortController?.current?.abort) {
+      abortController?.current.abort();
+    }
+  };
+
+  const handleSearch = async () => {
+    const searchValue = search.trim();
+    setSearch("");
+    if (loading) return;
+    setLoading(true);
+    abortController.current = new AbortController();
+    abortController.current.signal.addEventListener("abort", () => {
+      setLoading(false);
+      setAnswer("");
+    });
+    // const url = `${}`;
+
+    const response = await fetch("", {
+      method: "POST",
+      signal: abortController.current?.signal,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: "/blitz " + searchValue,
+      }),
+    });
+    if (!response.ok) {
+      alert("Something went wrong, please try again later.");
+      setLoading(false);
+      return;
+    }
+    if (!response?.headers?.get("content-type")) {
+      const text = await response.text();
+      setAnswer(text);
+      setLoading(false);
+      setTimeout(() => {
+        setAnswer("");
+      }, 10000);
+      return;
+    }
+
+    const reader = response?.body?.getReader();
+    let decoder = new TextDecoder();
+
+    let message = "";
+    while (true && reader) {
+      const chunkReader = await reader.read();
+      console.log({ chunkReader });
+      const { done, value } = chunkReader;
+      if (done) {
+        break;
+      }
+
+      const text = decoder.decode(value);
+      const lines = text.trim().split("\n\n\n\n\n");
+
+      for (const line of lines) {
+        const eventData = JSON.parse(line);
+        if (eventData?.sources) continue;
+        if (eventData.error) {
+          // if eventData has error
+          console.log("error ->", eventData.error);
+          setLoading(false);
+          // show error
+          alert(eventData.error);
+          return;
+        }
+        message = message + eventData.value;
+        setAnswer(message);
+      }
+    }
+    setAnswer(message);
+
+    setLoading(false);
+    setTimeout(() => {
+      setAnswer("");
+    });
+  };
+
+  useEffect(() => {
+    if (
+      !chats[chats.length - 1]?.isSender &&
+      chats[chats.length - 1]?.msg === ""
+    )
+      handleSearch();
+  }, [chats]);
+
+  useEffect(() => {
+    scrollToBottom();
+    if (answer !== "" && chats.length) {
+      let tempChats = [...chats];
+      tempChats[tempChats.length - 1].msg = answer;
+      setChats(tempChats);
+    }
+  }, [answer, loading]);
   return (
     <div className="w-screen h-screen">
       <div className="flex flex-col h-full justify-between max-w-[1100px] mx-auto prose-nbx p-[16px]">
         <div className="chatsHolder w-full flex flex-col gap-[16px] overflow-scroll pb-[16px]">
-          {/* messagecard */}
+          {chats?.map((chat, id) => (
+            <MessageCard
+              chat={chat}
+              key={id}
+              loading={
+                id === chats?.length - 1
+                  ? !chats?.[chats?.length - 1]?.msg && loading
+                  : false
+              }
+            />
+          ))}
         </div>
 
         <div className="relative">
@@ -62,7 +186,7 @@ export default function Home() {
                       {
                         msg: "",
                         isSender: false,
-                        sender: "BlitzScaling",
+                        sender: "TuneStudio",
                       },
                     ]);
                   }}
@@ -97,7 +221,7 @@ export default function Home() {
                   {
                     msg: "",
                     isSender: false,
-                    sender: "BlitzScaling",
+                    sender: "TuneStudio",
                   },
                 ]);
               }
@@ -116,7 +240,10 @@ export default function Home() {
                   </div>
                 </div>
               ) : (
-                <div className="w-[16px] flex mb-[8px] items-center text-light-icon-base dark:text-dark-icon-base hover:dark:text-dark-icon-hover hover:text-light-icon-hover">
+                <div
+                  onClick={() => stopGenerating()}
+                  className="w-[16px] flex mb-[8px] items-center text-light-icon-base dark:text-dark-icon-base hover:dark:text-dark-icon-hover hover:text-light-icon-hover"
+                >
                   <Pause />
                 </div>
               )
@@ -138,14 +265,14 @@ export default function Home() {
                 {
                   msg: "",
                   isSender: false,
-                  sender: "BlitzScaling",
+                  sender: "TuneStudio",
                 },
               ]);
             }}
           />
           <div className="w-full text-center">
             <span className="text-light-text-base dark:text-dark-text-base mini">
-              Powered by <a href="https://chat.tune.app/">TuneChat</a>
+              Powered by <a href="https://studio.rc.tune.app/">TuneStudio</a>
             </span>
           </div>
         </div>
@@ -153,3 +280,45 @@ export default function Home() {
     </div>
   );
 }
+
+const MessageCard = ({
+  chat,
+  loading,
+}: {
+  chat: ChatInterface;
+  loading: boolean;
+}) => {
+  return (
+    <div className="flex gap-[16px] w-full">
+      <div className="h-[30px] w-[30px] flex justify-center items-center rounded-full bg-light-background-surfaceHighHover dark:bg-dark-background-surfaceHighHover">
+        <div
+          className="min-w-[38px] bg-[#FF6A1F] medium-pl h-[38px] w-[38px] scale-[0.67] flex rounded-full"
+          style={{ background: "#FF6A1F" }}
+        ></div>
+      </div>
+      <div className="p-[8px] rounded-md w-full bg-light-background-surfaceLow dark:bg-dark-background-surfaceHigh flex flex-col">
+        <span className="mini text-light-text-subtle dark:text-dark-text-subtle ">
+          {chat?.sender}
+        </span>
+        {!loading ? (
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehype]}
+            className={`markdownHolder`}
+            components={{
+              table: (props: any) => (
+                <div className="overflow-x-auto w-full">
+                  <table {...props} className="table-auto w-full" />
+                </div>
+              ),
+            }}
+          >
+            {chat.msg}
+          </ReactMarkdown>
+        ) : (
+          <div className="h-[16px] w-[2px] bg-light-text-subtle dark:bg-dark-text-subtle animate-pulse"></div>
+        )}
+      </div>
+    </div>
+  );
+};
