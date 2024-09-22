36432b4c1f0711ac5047f324622af43e085787ec
Shashank Prakash <shashank404error@shashank404error.local>
2024-09-22 10:01:38 +0530

    masking masking masking

--- community/mailGPT/tune_ai.py
+++ community/mailGPT/tune_ai.py
@@ -5,7 +5,7 @@ def run_tune_ai(user_content,query):
   stream = False
   url = "https://proxy.tune.app/chat/completions"
   headers = {
-      "Authorization": "sk-tune-iXTmtQmtRyEYYHIfuNywyHr89luzrmcEaqR",
+      "Authorization": "",
       "Content-Type": "application/json",
   }
   data = {
