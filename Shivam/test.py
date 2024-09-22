bed288b226cff033105c1ffe9c9c914cb5578761
Shivam Sinha <45197793+Shivam-026@users.noreply.github.com>
2024-09-22 09:45:55 +0530

    Update test.py

--- Shivam/test.py
+++ Shivam/test.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from generator import generate_code
+st.title('Code Generator App')
 
+tuneai_api_key = st.sidebar.text_input('TuneAI API Key', type='password')
+
+with st.form('code_generator_form'):
+    user_prompt = st.text_area("Enter your input")
+
+
+    submit_button = st.form_submit_button('Submit')
+
+if submit_button:
+    if tuneai_api_key == "":
+        st.warning('Enter your TuneAI API key!', icon='⚠')
+    elif user_prompt == "":
+        st.warning('Please enter a prompt!', icon='⚠')
+    else:
+        with st.spinner('Generating code...'):
+            generated_code = generate_code(user_prompt, tuneai_api_key)
+            st.code(generated_code)
