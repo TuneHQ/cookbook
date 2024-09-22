11b359b1794aec6b3b65acf06738679701d79c0e
Shuvrajyoti Paul <33157366+shuvro09@users.noreply.github.com>
2024-09-22 10:02:06 +0530

    Update app.py

--- tic_tac_toe/app.py
+++ tic_tac_toe/app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from generator import generate_code
-st.title('Code Generator App')
+from player import play_game
+st.title('Play Tic Tac Toe')
 
 tuneai_api_key = st.sidebar.text_input('TuneAI API Key', type='password')
 
-with st.form('code_generator_form'):
+with st.form('tic_tac_toe_form'):
     user_prompt = st.text_area("Enter your prompt")
     
    
@@ -17,5 +17,5 @@ if submit_button:
         st.warning('Please enter a prompt for playing Tic Tac Toe', icon='⚠')
     else:
         with st.spinner('Searching for a move...'):
-            generated_code = generate_code(user_prompt, tuneai_api_key)
-            st.code(generated_code)
+            response = play_game(user_prompt, tuneai_api_key)
+            st.code(response)
