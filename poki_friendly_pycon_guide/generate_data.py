743ddb1ef2f0aa5805611801c2127aa83c243fb0
khadeom <khaeom10@gmail.com>
2024-09-22 13:46:07 +0530

    add poki

---
+++ poki_friendly_pycon_guide/generate_data.py
@@ -0,0 +1,5 @@
+from rag.utils import extract_website_data, create_embeddings
+import time
+extract_website_data("https://in.pycon.org/cfp/2024/proposals/", start_time=time.time())
+# extract_website_data("https://in.pycon.org/2024/", start_time=time.time())
+# create_embeddings("/Users/omkhade/Projects/pycon/poki_friendly_pycon_guide/pycon_website_2.txt") 
